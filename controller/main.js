7371549463ece9bc8eda4de4f8f22e463577ccd3
Chiru Man <admin@chirumer.com>
2023-07-31 00:13:08 +0530

    auth window style

--- controller/main.js
+++ controller/main.js
@@ -1,30 +1,36 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-
-  win.loadFile('index.html')
+function createAuthWindow() {
+    const win = new BrowserWindow({
+        center: true,
+        width: 400,
+        height: 200,
+        maximizable: false,
+        minimizable: false,
+        resizable: false,
+        closable: false,
+        frame: false,
+        alwaysOnTop: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    win.loadFile(path.join(__dirname, 'pages', 'auth_window', 'index.html'));
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    createAuthWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createAuthWindow();
+        }
+    });
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
